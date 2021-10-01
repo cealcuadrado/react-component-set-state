e5c36370bae46935924fbe2178cd338df268d194
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-01 19:23:11 -0300

    Definido layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
 class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: []
+        }
+
+        this.setStateHandler = this.setStateHandler.bind(this);
+    }
+
+    setStateHandler() {
+        var item = 'setState...';
+        var myArray = this.state.data.slice();
+        myArray.push(item);
+        this.setState({ data: myArray });
+    }
+
     render() {
         return (
             <div>
-                <p>Hola, mundo!</p>
+                <button className="btn btn-primary" onClick={this.setStateHandler}>
+                    Press me to set state
+                </button>
+                <hr />
+                <p>State: {this.state.data}</p>
             </div>
         );
     }
